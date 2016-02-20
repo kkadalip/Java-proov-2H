ad78e45ffcb955e27a5faff9eb830b8a30d50b65
karlk <karlk@DESKTOP-VHFKDKV.lan>
2016-02-20 04:27:10 +0200

    "Default" controller now displays correct jsp as a test.

--- src/controller/Default.java
+++ src/controller/Default.java
@@ -22,7 +22,8 @@ public class Default extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Controller Default doGet");
 		//doStuff(request);
-		request.getRequestDispatcher("WEB-INF/index.jsp").forward(request, response);
+		//request.getRequestDispatcher("WEB-INF/index.jsp").forward(request, response);
+		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
